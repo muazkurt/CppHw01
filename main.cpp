c1930b3b6720eb2a5ecf37aeb2554d21501db035
Muaz Kurt <muazkurt@gmail.com>
2017-09-26 21:39:17 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -15,37 +15,92 @@
 using namespace std;
 
 
-struct map
-{
-    bool fill;
-    int user;
-};
 
 
-map ** CreateMap(map ** board, int size)
+int ** Createint(int ** board, int size);
+
+int ** FillEmpty(int ** board, int size);
+
+int ** MakeBoard(int ** board, int size);
+
+char lover(char x);
+
+//Making Move
+int PositionY(const int ** board, int positionX, int size);
+
+int * getInput(const int ** board, int * position, int size);
+
+int ** MakeMove(int ** board, int user, const int position[2])
+
+//Printing int and componnents
+void printHead(int size);
+
+void printBody(const int ** printable, int size);
+
+void printint(const int ** printable, int size);
+
+bool IsCheckable(const int ** board, const int position[2], int size, int line);
+
+bool checkHorisontal(const int ** board, const int position[2], int size)
+
+bool leftUp(const int ** board, const int position[2])
+
+bool leftDown(const int ** board, const int position[2]);
+
+bool checkLeft(const int ** board, const int position[2], int size);
+
+bool RightUp(const int ** board, const int position[2]);
+
+bool RightDown(const int ** board, const int position[2]);
+
+bool checkRight(const int ** board, const int position[2], int size);
+
+bool WinSituation(const int ** board, int position[2], int size);
+
+void freeint(int ** board, int size);
+
+int main(int argc, char * argv[])
 {
-    board = new map*[size];
+	int ** board;
+        int size = 0;
+        if(argc > 1)
+            size = atoi(argv[1]);
+        size = 10;
+        board = MakeBoard(board, size);
+        int x[2];
+	do {
+            printint((const int **)board, size);
+            getInput((const int **)board, x, size);
+            board = MakeMove(board, 1, x);
+            printint((const int **)board, size);
+        } while(WinSituation((const int **)board, x, size) == false);
+        freeint(board, size);
+	return 0;
+}
+
+
+
+int ** Createint(int ** board, int size)
+{
+    board = new int*[size];
     for(int i = 0; i < size; ++i)
-        board[i] = new map[size];
+        board[i] = new int[size];
     return board;
 }
 
 
-map ** FillEmpty(map ** board, int size)
+int ** FillEmpty(int ** board, int size)
 {
     for(int i = 0; i < size; ++i)
         for(int j = 0; j < size; ++j)
-        {
-            board[i][j].fill = false;
-            board[i][j].user = 0;
-        }
+            board[i][j] = 0;
     return board;
 }
 
 
-map ** MakeBoard(map ** board, int size)
+int ** MakeBoard(int ** board, int size)
 {
-    board = CreateMap(board, size);
+    board = Createint(board, size);
     board = FillEmpty(board, size);
     return board;
 }
@@ -61,14 +116,14 @@ char lover(char x)
 
 
 //Making Move
-int PositionY(map ** board, int positionX, int size)
+int PositionY(const int ** board, int positionX, int size)
 {
     int i = size - 1;
-    for(i; board[i][positionX].fill == true && i >= 0; --i);
+    for(i; board[i][positionX] > 0 && i >= 0; --i);
     return i;
 }
 
-int * getInput(map ** board, int * position, int size)
+int * getInput(const int ** board, int * position, int size)
 {
     position[1] = -1;
     do{
@@ -90,15 +145,17 @@ int * getInput(map ** board, int * position, int size)
 
 
 
-map ** MakeMove(map ** board, int position[2])
+int ** MakeMove(int ** board, int user, const int position[2])
 {
-    board[position[0]][position[1]].fill = true;
-    board[position[0]][position[1]].user = 1;
+    if(user = 1)
+        board[position[0]][position[1]] = 1;
+    else
+        board[position[0]][position[1]] = 2;
     return board;
 }
 
 
-//Printing map and componnents
+//Printing int and componnents
 void printHead(int size)
 {
 		char a = 'a';
@@ -113,21 +170,25 @@ void printHead(int size)
 }
 
 
-void printBody(map ** printable, int size)
+void printBody(const int ** printable, int size)
 {
 		for(int i = 0; i < size; ++i)
 		{
 			for(int j = 0; j < size; ++j)
 			{
-                            if(printable[i][j].fill == true) 
-                            {
-                                if(printable[i][j].user == 1)
-                                        cout << 'X';
-                                    else cout << 'O';
-                            }
-                            else
-                                cout << "-";
-                            cout << " ";
+                switch(printable[i][j]) 
+                {
+                    case 1:
+                        cout << 'X';
+                        break;
+                    case 2:
+                        cout << 'O';
+                        break;
+                    default:
+                        cout << '-';
+                        break;
+                }
+                cout << ' ';
 			}
 			cout << endl;
 		}
@@ -135,7 +196,7 @@ void printBody(map ** printable, int size)
 }
 
 
-void printmap(map ** printable, int size)
+void printint(const int ** printable, int size)
 {
 		printHead(size);
 		printBody(printable, size);
@@ -143,40 +204,41 @@ void printmap(map ** printable, int size)
 }
 
 
-bool IsCheckable(map ** board, int position[2], int size, int line)
+bool IsCheckable(const int ** board, const int position[2], int size, int line)
 {
     bool Legal = false;
     switch (line)
     {
         case 0:
-            if(size - 1 - position[0] > 3)
+            if(size - 1 - position[0] > 2)
                 Legal = true;
             break;
-        /*
+        //for right-down
         case 1:
-            if(position[0] > 2)
+            if(size - 1 - position[0] > 3 || size - 1 - position[0] > 3);
             {
                 int i = 0;
                 while(i != -1 && i < 3)
                 {
-                    if(board[position[1]][position[0]].fill == true &&
-                       board[position[1] - (i + 1)][position[0] - (i + 1)].fill == true)
+                    if(board[position[1]][position[0]] > 0 &&
+                       board[position[1] + (i + 1)][position[0] + (i + 1)] > 0)
                         ++i;
                     else
                         i = -1;
                 }
                 if (i < 3)
-                    Legal = true;
+                   Legal = true;
             }
             break;
+        //for right-up
         case 2:
-            if(size - 1 - position[0] > 3);
+            if(size - 1 - position[1] > 3);
             {
                 int i = 0;
                 while(i != -1 && i < 3)
                 {
-                    if(board[position[1]][position[0]].fill == true &&
-                       board[position[1] + (i + 1)][position[0] + (i + 1)].fill == true)
+                    if(board[position[1]][position[0]] > 0 &&
+                       board[position[1] - (i + 1)][position[0] + (i + 1)] > 0)
                         ++i;
                     else
                         i = -1;
@@ -185,116 +247,154 @@ bool IsCheckable(map ** board, int position[2], int size, int line)
                     Legal = true;
             }
             break;
-         */
+            
+        //For left-up
+        case 3:
+            if(position[1] > 2)
+            {
+                int i = 0;
+                while(i != -1 && i < 3)
+                {
+                    if(board[position[1]][position[0]] > 0 &&
+                       board[position[1] - (i + 1)][position[0] - (i + 1)] > 0)
+                        ++i;
+                    else
+                        i = -1;
+                }
+                if (i < 3)
+                    Legal = true;
+            }
+            break;
+        //for left-down
+        case 4:
+            if(position[1] > 2 && size -1 - position[0] > 3)
+            {
+                int i = 0;
+                while(i != -1 && i < 3)
+                {
+                    if(board[position[1]][position[0]] > 0&&
+                       board[position[1] - (i + 1)][position[0] - (i + 1)] > 3)
+                        ++i;
+                    else
+                        i = -1;
+                }
+                if (i < 3)
+                    Legal = true;
+            }
+            break;
         default:
             break;
     }
     return Legal;
 }
 
-bool checkHorisontal(map ** board, int position[2], int size)
+bool checkHorisontal(const int ** board, const int position[2], int size)
 {
     bool Legal = false;
     if(IsCheckable(board, position, size, 0) == true)
     {
         int i = 0;
-        while(i != -1 && i < 3)
-        {
-        if(board[position[0]][position[1]].user == board[position[0]][position[1]].user)
-                ++i;
-            else i = -1;
-        }
-        if(i == 3)
-            Legal == true;
+        if ((board[position[0]][position[1]] == board[position[0] + 1][position[1]]) &&
+                (board[position[0] + 1][position[1]]
+                == board[position[0] + 2][position[1]]) &&
+                (board[position[0] + 2][position[1]]
+                == board[position[0] + 3][position[1]]))
+            Legal = true;
     }
     return Legal;
 }
 
 
-bool checkLeft(map ** board, int position[2], int size)
+bool leftUp(const int ** board, const int position[2])
 {
     bool Legal = false;
-    if(IsCheckable(board, position, size, 1) == true)
-    {
-        int i = 0;
-        for(; i != -1 && i < 3; ++i)
-        {
-            if(board[position[1] - i][position[0] - i].user != board[position[1] - (i + 1)][position[0] - (i + 1)].user)
-                i = -1;
-        }
-        if(i == 3)
-            Legal == true;
-    }
+    if(
+            (board[position[0]][position[1]] 
+            == board[position[0] - 1][position[1] - 1]) &&
+                (board[position[0] - 1][position[1] - 1]
+                == board[position[0] - 2][position[1] - 2]) &&
+                (board[position[0] - 2][position[1] - 2]
+                == board[position[0] - 3][position[1] - 3]))
+        Legal = true;
     return Legal;
 }
 
 
-bool checkRight(map ** board, int position[2], int size)
+bool leftDown(const int ** board, const int position[2])
 {
     bool Legal = false;
-    if(IsCheckable(board, position, size, 2) == true)
-    {
-        int i = 0;
-        for(; i != -1 && i < 3; ++i)
-        {
-            if(board[position[1] - i][position[0] + i].user != board[position[1] - (i + 1)][position[0] + (i + 1)].user)
-                i = -1;
-        }
-        if(i == 3)
-            Legal == true;
-    }
+    if(
+            (board[position[0]][position[1]] 
+            == board[position[0] + 1][position[1] - 1]) &&
+                (board[position[0] + 1][position[1] - 1]
+                == board[position[0] + 2][position[1] - 2]) &&
+                (board[position[0] + 2][position[1] - 2]
+                == board[position[0] + 3][position[1] - 3]))
+        Legal = true;
     return Legal;
 }
 
 
-bool WinSituation(map ** board, int position[2], int size)
+bool checkLeft(const int ** board, const int position[2], int size)
 {
-    if(checkHorisontal(board, position, size) == true
-           /* || checkLeft(board, location) == true
-            || checkRight(board, location) == true*/)
+    bool Legal = false;
+    if(IsCheckable(board, position, size, 3) == true)
+        Legal = leftUp(board, position);
+    if(Legal == false && IsCheckable(board, position, size, 4) == true)
+        Legal = leftDown(board, position);
+    return Legal;
+}
+
+
+bool RightUp(const int ** board, const int position[2])
+{
+    bool Legal = false;
+    if(
+            (board[position[0]][position[1]] 
+            == board[position[0] - 1][position[1] + 1]) &&
+                (board[position[0] - 1][position[1] + 1]
+                == board[position[0] - 2][position[1] + 2]) &&
+                (board[position[0] - 2][position[1] + 2]
+                == board[position[0] - 3][position[1] + 3]))
+        Legal = true;
+    return Legal;
+}
+
+bool RightDown(const int ** board, const int position[2])
+{
+    if((board[position[0]][position[1]] == board[position[0] + 1][position[1] + 1]) &&
+                (board[position[0] + 1][position[1] + 1]
+                == board[position[0] + 2][position[1] + 2]) &&
+                (board[position[0] + 2][position[1] + 2]
+                == board[position[0] +  3][position[1] + 3]))
         return true;
     return false;
 }
 
-void freemap(map ** board)
+
+bool checkRight(const int ** board, const int position[2], int size)
 {
-    for(int i = 0; i < SIZE; ++i)
-        delete board[SIZE];
+    bool Legal = false;
+    if(IsCheckable(board, position, size, 1) == true)
+        Legal = RightDown(board, position);
+    if(Legal == false && IsCheckable(board, position, size, 2) == true)
+        Legal = RightUp(board, position);
+    return Legal;
+}
+
+
+bool WinSituation(const int ** board, int position[2], int size)
+{
+    if(/*checkHorisontal(board, position, size) == true
+            || checkLeft(board, position, size) == true
+            || */checkRight(board, position, size) == true)
+        return true;
+    return false;
+}
+
+void freeint(int ** board, int size)
+{
+    for(int i = 0; i < size; ++i)
+        delete board[size];
     delete[] board;
 }
-/*
-a b c d
-- - - -
-- - - -
-- - - -
-- - - -
-*/
-
-
-/*
-first we need square board to play game.
-Score
-Print
- * Input search
-
-
-*/
-
-
-int main(int argc, char * argv[2])
-{
-	map ** board;
-        int size = atoi(argv[1]);
-        size = 10;
-        board = MakeBoard(board, size);
-        int x[2];
-	do {
-            printmap(board, size);
-            getInput(board, x, size);
-            board = MakeMove(board, x);
-            printmap(board, size);
-        } while(WinSituation(board, x, size) == false);
-        freemap(board);
-	return 0;
-}
-
